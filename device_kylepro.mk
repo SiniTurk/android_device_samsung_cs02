0bdc3604e58719788e853a8409ef66a714cf8a08
SandPox <sandpox@protonmail.ch>
2016-05-20 02:28:45 +0000

    Use multi-thread for Dex2Oat

--- device_kylepro.mk
+++ device_kylepro.mk
@@ -136,6 +136,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog
 
+# Dex2Oat multi-thread
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.dex2oat_thread_count=2
+
 # Use Awesomeplayer
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.media.use-awesome=true
