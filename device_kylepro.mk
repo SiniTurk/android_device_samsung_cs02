47e2973a07c0c0f737ebb5ee99654e233d417a2b
SandPox <sandpox@protonmail.ch>
2016-05-27 07:38:11 +0000

    Remove: Disable dex2oat watchdog
    * Back in the day, dex2oat has been limit optimizing app timeout to
    10 minutes so if app is not getting optimized successfuly it will retry
    to optimize it on next boot therefore caused "Optimizing apps" on every
    boot so we need to disable watchdog to prevent dex2oat get killed if
    reached the timeout.
    
    * Now Google has increase the timeout to 60 minutes on Android 6.0.1_r3
    so it should having more than enough to optimizing app.

--- device_kylepro.mk
+++ device_kylepro.mk
@@ -136,10 +136,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.checkjni=0 \
     dalvik.vm.checkjni=false
 
-# ART
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-flags=--no-watch-dog
-
 # Dex2Oat multi-thread
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.dex2oat_thread_count=2
