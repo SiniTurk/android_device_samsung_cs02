3899d263e5d8bd6cb10af11d1bc91dd93cebb478
ishantvivek <ishantvivek1311@gmail.com>
2016-10-08 09:58:28 +0000

    Update healthd for Android N

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -18,5 +18,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd-hawaii.cpp
 LOCAL_MODULE := libhealthd.hawaii
-LOCAL_C_INCLUDES := system/core/healthd
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_LIBRARIES := libhealthd.default
 include $(BUILD_STATIC_LIBRARY)
