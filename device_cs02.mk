92ce3ba37dbae8f7e1ed639cab84e6d6bbe91b8d
Tim Schumacher <timschumi2@arcor.de>
2016-10-14 15:52:38 +0200

    Changed vendor file path

--- device_cs02.mk
+++ device_cs02.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/cs02/cs02-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/cs02/cs02-vendor.mk)
 
 # Use high-density artwork where available
 PRODUCT_LOCALES += hdpi
