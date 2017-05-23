a6c6e67d53e6bbff0e862ee7fd5bd0c834fbdd75
Tim Schumacher <timschumi2@arcor.de>
2017-05-23 22:19:21 +0200

    Remove custom toolchain
    Change-Id: I257df6d6a9b6400bb422f792dc45b5bcbcce5f70

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,8 +26,6 @@ BOARD_KERNEL_BASE := 0x82000000
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_CONFIG := bcm21664_hawaii_ss_cs02_rev02_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/cs02
-# Make sure you have the correct toolchain available
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
 
 # Partition sizes
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608 # mmcblk0p5, 8192 blocks
