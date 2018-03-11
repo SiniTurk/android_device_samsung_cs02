bd8b48b9ab3d670bba52aa27d01fa55e7f1a113f
Tim Schumacher <timschumi@gmx.de>
2018-03-11 17:34:10 +0100

    Enforce presence of the vendor tree
    Change-Id: I30dc76619884b71d2d60c270c5b9424d78d0a0c0

--- device_cs02.mk
+++ device_cs02.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/cs02/cs02-common-vendor.mk)
+$(call inherit-product, vendor/samsung/cs02/cs02-common-vendor.mk)
 
 # Use high-density artwork where available
 PRODUCT_LOCALES += hdpi
