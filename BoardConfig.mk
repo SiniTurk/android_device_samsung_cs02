b09f447128c8c3694161fb817558651176b52771
Tim Schumacher <timschumi@gmx.de>
2018-03-31 15:54:51 +0200

    Disable dlmalloc
    dlmalloc reports heap corruptions all the time, which don't seem
    to be there with the default malloc
    
    Change-Id: I562905a60124b8815eb3fd1b7f0b1d9ca2da91c5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,9 +139,6 @@ TARGET_SPECIFIC_HEADER_PATH := device/samsung/cs02/include
 # MTP
 BOARD_MTP_DEVICE := /dev/mtp_usb
 
-# jemalloc causes a lot of random crash on free()
-MALLOC_IMPL := dlmalloc
-
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     device/samsung/cs02/sepolicy
